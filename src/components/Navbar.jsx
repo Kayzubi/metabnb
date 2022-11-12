23998adc1d2398f7377ddf22e69780ba22cd6150
Kizito Azubuike <kayzubi84@gmail.com>
2022-11-12 01:42:00 +0100

    feat: add navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
+import { ReactComponent as Navlogo } from './assets/navlogo.svg'
+
 function Navbar() {
-  return <h1>Navbar</h1>
+  return (
+    <div className='navbar'>
+      <div className='navbar__brand'>
+        <Navlogo />
+      </div>
+      <div className='navbar__toggle'></div>
+      <div className='navbar__links'>
+        <p className='navbar__links-item'>Home</p>
+        <p className='navbar__links-item'>Place to stay</p>
+        <p className='navbar__links-item'>NFTs</p>
+        <p className='navbar__links-item'>Community</p>
+      </div>
+      <div className='navbar__button'>
+        <button className='btn btn__connect'> Connect wallet</button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
