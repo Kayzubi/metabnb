d7919c7f004c4e3bec103c16ba0b9a020fd79adc
Kizito Azubuike <kayzubi84@gmail.com>
2022-11-12 03:19:40 +0100

    style: navbar mobile styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,18 @@ function Navbar() {
       <div className='navbar__brand'>
         <Navlogo />
       </div>
-      <div className='navbar__toggle'></div>
-      <div className='navbar__links'>
-        <p className='navbar__links-item'>Home</p>
-        <p className='navbar__links-item'>Place to stay</p>
-        <p className='navbar__links-item'>NFTs</p>
-        <p className='navbar__links-item'>Community</p>
-      </div>
-      <div className='navbar__button'>
-        <button className='btn btn-primary'> Connect wallet</button>
-      </div>
+      <nav>
+        <div className='navbar__toggle'></div>
+        <ul className='navbar__links'>
+          <li className='navbar__links-item'>Home</li>
+          <li className='navbar__links-item'>Place to stay</li>
+          <li className='navbar__links-item'>NFTs</li>
+          <li className='navbar__links-item'>Community</li>
+        </ul>
+        <div className='navbar__button'>
+          <button className='btn btn-primary'> Connect wallet</button>
+        </div>
+      </nav>
     </div>
   )
 }
